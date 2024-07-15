59c330f33364f99c6a91c17557614304dc89ff3e
MircoPilcoPeralta <mircoestefanopilcoperalta@gmail.com>
2024-07-15 19:59:27 -0400

    imported project

---
+++ .eslintrc.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:jsx-a11y/recommended',
+    'eslint-config-prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs'],
+  parser: '@typescript-eslint/parser',
+  plugins: ['react-refresh', 'jsx-a11y'],
+  rules: {
+    'react-refresh/only-export-components': [
+      'warn',
+      { allowConstantExport: true },
+    ],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+}
